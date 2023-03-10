a8d455371b23338cd742f7e67ba69020d914a8f2
Aboobacker P <aboobackerp@gmail.com>
2023-03-10 12:36:51 +0400

    Update api.php

--- capi_api/api.php
+++ capi_api/api.php
@@ -2,12 +2,9 @@
 require_once("../../../../wp-load.php");
 
 if( !isset($_POST['event_name']) ) exit;
-
 $event_name = $_POST['event_name'];
 $event_source_url = $_POST['event_source_url'];
-
 $event_id = $fbp = $external_id = '';
-
 if(get_field('deduplication_method','option') == 'event_based'){
     $event_id = isset($_POST['event_id']) ? $_POST['event_id'] : '';
 }
@@ -15,12 +12,10 @@ else if(get_field('deduplication_method','option') == 'external_id'){
     $fbp = isset($_POST['fbp']) ? $_POST['fbp'] : '';
     $external_id = isset($_POST['external_id']) ? hash('sha256', $_POST['external_id']) : '';
 }
-
 $event_time = time();
 $action_source = "website";
 $client_ip_address = file_get_contents('https://secure.thefirstgroup.com/abipapi.php');
 $client_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
-
 $pixel_id = get_field('pixel_id', 'option');
 $access_token = get_field('access_token', 'option');
 $test_event_code = get_field('test_event_code', 'option') ? get_field('test_event_code', 'option') : '';
