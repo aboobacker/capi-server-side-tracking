a466f80f574fa9ce828fab8b6aa7d2536f4b5351
Aboobacker P <aboobackerp@gmail.com>
2023-03-20 10:35:48 +0400

    Added condition to check if ACF PRO already installed

--- capi-server-side-tracking.php
+++ capi-server-side-tracking.php
@@ -3,7 +3,7 @@
 * Plugin Name: Server Side Tracking for CAPI
 * Plugin URI: https://www.aboobacker.com/
 * Description: This plugin helps to impliment server side tracking for Facebook Conversion API (CAPI).
-* Version: 1.2
+* Version: 1.3
 * Author: Aboobacker P
 * Author URI: https://www.aboobacker.com/
 **/
@@ -16,19 +16,22 @@ define( 'MY_ACF_PATH', $plugins_path . '/includes/acf/' );
 define( 'MY_ACF_URL', $plugins_url . '/includes/acf/' );
 
 // Include the ACF plugin.
-include_once( MY_ACF_PATH . 'acf.php' );
+//  The ACF class doesn't exist, so you can probably redefine your functions here
+if (!class_exists('ACF')) {
+    include_once( MY_ACF_PATH . 'acf.php' );
+
+    // (Optional) Hide the ACF admin menu item.
+    add_filter('acf/settings/show_admin', '__return_false');
+
+    // When including the PRO plugin, hide the ACF Updates menu
+    add_filter('acf/settings/show_updates', '__return_false', 100);
+}
 
 // Customize the url setting to fix incorrect asset URLs.
 add_filter('acf/settings/url', 'my_acf_settings_url');
 function my_acf_settings_url( $url ) {
     return MY_ACF_URL;
 }
-
-// (Optional) Hide the ACF admin menu item.
-add_filter('acf/settings/show_admin', '__return_false');
-
-// When including the PRO plugin, hide the ACF Updates menu
-add_filter('acf/settings/show_updates', '__return_false', 100);
 
 require_once('functions/field-settings.php');
 require_once('functions/actions.php');
