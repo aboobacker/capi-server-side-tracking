1cbca20f8caca2388714d55cbaf1484066485bbe
Aboobacker P <aboobackerp@gmail.com>
2023-04-25 10:50:14 +0400

    2.5

--- capi-server-side-tracking.php
+++ capi-server-side-tracking.php
@@ -3,7 +3,7 @@
 * Plugin Name: Server Side Tracking for CAPI
 * Plugin URI: https://www.aboobacker.com/
 * Description: This plugin helps to impliment server side tracking for Facebook Conversion API (CAPI).
-* Version: 2.4
+* Version: 2.5
 * Author: Aboobacker P
 * Author URI: https://www.aboobacker.com/
 **/
